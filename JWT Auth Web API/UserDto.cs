f551a9a8663c061dd5a208128958e70ceb114664
hkpnrr <halilakpinar4115@gmail.com>
2023-03-05 20:26:43 +0300

    Add project files.

---
+++ JWT Auth Web API/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace JWT_Auth_Web_API
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+
+    }
+}
